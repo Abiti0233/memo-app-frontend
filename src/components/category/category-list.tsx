2900bb5707750794f7b7bc8de8a2848cb1cfb1f1
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2024-12-21 18:03:42 +0900

    Add: sidebarのところをコンポーネント化

---
+++ src/components/category/category-list.tsx
@@ -0,0 +1,31 @@
+import { type Category } from "@/types/category/category";
+import CategoryItem from "./category-item";
+
+export default function CategoryList({
+	category,
+	className,
+}: {
+	category: Category;
+	className?: string;
+}) {
+	return (
+		<div className={className}>
+			<ul className="flex flex-col gap-8 align-middle">
+				<li>
+					<CategoryItem name="メモ一覧" />
+				</li>
+				<li>
+					<CategoryItem name="ブックマーク" />
+				</li>
+				<li>
+					<CategoryItem name="最近削除した項目" />
+				</li>
+				{category.map((category) => (
+					<li key={category.id}>
+						<CategoryItem name={category.name} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
