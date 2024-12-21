3168cfe70a3523f33239b4e1c853f60e28266c45
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2024-12-21 18:04:17 +0900

    Add: サンプルデータの型を定義

---
+++ src/types/category/category.ts
@@ -0,0 +1,9 @@
+// 実際のデータはcreatedAt,updatedAtが日付型であるが、一旦文字列型で扱う
+// TODO: 実際のデータを扱うときはcreatedAtとupdatedAtを日付型に変換する
+export type Category = {
+	id: string;
+	userId: string;
+	name: string;
+	createdAt: string;
+	updatedAt: string;
+}[];
