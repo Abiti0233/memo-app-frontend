8a268dc1769f7adc345aaf5dca10a472ecc60fa0
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2024-12-21 19:27:09 +0900

    Fix: memo一覧のところをコンポーネントに置き換え

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,9 @@
 import SideBar from "@/components/layout/side-bar";
+import MemoList from "@/components/memo/memo-list";
+import { getMemos } from "@/data/memo";
 
 export default async function Page() {
+	const memos = await getMemos();
 	return (
 		<main>
 			<div className="flex">
@@ -19,79 +22,7 @@ export default async function Page() {
 						</div>
 						<div className="flex">
 							<div className="h-screen w-[35%] overflow-y-scroll bg-white">
-								<div className="px-6 py-10">
-									{/* コンポーネント化してね */}
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-									<div className="flex flex-col gap-2 rounded-lg border-b border-solid border-grey-200 p-4 hover:bg-grey-100">
-										<h4 className="text-body-m-bold">タイトル</h4>
-										<div className="flex gap-4 text-caption-m">
-											<p>2024年3月11日</p>
-											<p>内容少し表示...</p>
-										</div>
-									</div>
-								</div>
+								<MemoList memos={memos} />
 							</div>
 							<div className="flex w-full flex-col px-10 pt-10">
 								<input
