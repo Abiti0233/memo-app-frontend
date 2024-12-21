8b8e8fcf2a71a4afeb7b2a520ebe4ae6a452b137
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2024-12-21 19:50:18 +0900

    Fix: headerの部分をコンポーネントに置き換え

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/layout/header";
 import SideBar from "@/components/layout/side-bar";
 import MemoList from "@/components/memo/memo-list";
 import { getMemos } from "@/data/memo";
@@ -10,20 +11,12 @@ export default async function Page() {
 				<SideBar />
 				<section className="w-full">
 					<div className="flex flex-col">
-						{/* //TODO: コンポーネント化してください。 */}
-						<div className="h-12 w-full bg-grey-300 py-2 pr-4">
-							<div className="ml-auto size-full w-[256px] pl-2">
-								<input
-									type="text"
-									className="size-full rounded-lg pl-4 text-caption-m"
-									placeholder="検索できるよ"
-								/>
-							</div>
-						</div>
+						<Header />
 						<div className="flex">
 							<div className="h-screen w-[35%] overflow-y-scroll bg-white">
 								<MemoList memos={memos} />
 							</div>
+							{/* メモ記入するところコンポーネント化する */}
 							<div className="flex w-full flex-col px-10 pt-10">
 								<input
 									type="text"
@@ -38,7 +31,6 @@ export default async function Page() {
 							</div>
 						</div>
 					</div>
-					{/* 右のメモ一覧＋メモ記入するところ */}
 				</section>
 			</div>
 		</main>
