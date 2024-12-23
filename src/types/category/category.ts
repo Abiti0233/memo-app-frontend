a61d9f3ed8d4172d37dd69c6f68ab4d75c4da107
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2024-12-23 19:33:18 +0900

    Fix: 不要な型定義を削除

--- src/types/category/category.ts
+++
@@ -1,9 +0,0 @@
-// 実際のデータはcreatedAt,updatedAtが日付型であるが、一旦文字列型で扱う
-// TODO: 実際のデータを扱うときはcreatedAtとupdatedAtを日付型に変換する
-export type Category = {
-	id: string;
-	userId: string;
-	name: string;
-	createdAt: Date;
-	updatedAt: Date;
-}[];
--- src/types/memo/memo.tsx
+++
@@ -1,9 +0,0 @@
-export type Memo = {
-	id: string;
-	userId: string;
-	title: string;
-	content: string;
-	is_archived: boolean;
-	createdAt: Date;
-	updatedAt: Date;
-}[];
