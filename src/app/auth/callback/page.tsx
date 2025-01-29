2a81ec879100729feb4b1fc24f0b9d0b566877a6
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2025-01-29 13:13:30 +0900

    Fix: buildエラー解消

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,24 +1,10 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Cookies from "js-cookie";
+import { Suspense } from "react";
+import Callback from "./callback";
 
 export default function Page() {
-	const router = useRouter();
-	const searchParams = useSearchParams();
-
-	useEffect(() => {
-		const token = searchParams.get("token");
-
-		if (token) {
-			const setToken = JSON.stringify(token);
-			Cookies.set("token", setToken);
-			router.push("/home");
-		} else {
-			router.push("/login");
-		}
-	}, [searchParams, router]);
-
-	return <div>認証中...</div>;
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<Callback />
+		</Suspense>
+	);
 }
